428bca8d24cae3678e71910dc598a38432fdc9fd
Тушь <106057273+Shimmershade@users.noreply.github.com>
2025-10-04 13:16:29 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char t[100];
+
+    int len = strlen(str);
+    int i = 0;
+
+    while (len > 0) t[i++] = str[len-- - 1];
+    t[i] = '\0';
+
+    strcpy(str, t);
 }
 
